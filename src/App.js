e8cdeeaa1952e3e1bf3b6bbd20bf6e5ce622fa46
Alexis Ortega <alexis.oc3@gmail.com>
2020-11-20 22:04:14 -0700

    Fix layout name

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "./App.css";
-import Layout from "./components/layout/Llayout";
+import Layout from "./components/layout/Layout";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import NotFound from "./pages/not-found/NotFound";
--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
-import "./Llayout.css";
+import "./Layout.css";
 import logo from "../../logo.svg";
 
 class Layout extends React.Component {
